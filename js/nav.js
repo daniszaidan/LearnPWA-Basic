33a18b109556bb434c49dec7cd2161c16c06896d
Danis Zaidan <daniszaidan@gmail.com>
2019-11-07 22:51:16 +0700

    Change XHTTP to Async Await

--- js/nav.js
+++ js/nav.js
@@ -1,58 +1,112 @@
+// const loadLoad = () => {
+//     fetch('/pages/about.html')
+//         .then(response => {
+//             return response.text();
+//         })
+//         .then(data => {
+//             return data.text();
+//         })
+//         .then()
+
+// }
+
+// loadLoad();
+
+
+loadPageContent = async page => {
+    const content = document.querySelector("#body-content");
+    const response = await fetch(`/pages/${page}.html`);
+    const { status } = response;
+
+    if (status === 200) {
+        const data = await response.text();
+        content.innerHTML = data;
+    } else if (status === 404) {
+        content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
+    } else {
+        content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
+    }
+}
+
+loadNavigation = async () => {
+    const response = await fetch('nav.html');
+    if (response.status !== 200) return;
+
+    const data = await response.text();
+    document.querySelectorAll(".topnav, .sidenav").forEach(function (elm) {
+        elm.innerHTML = data;
+    });
+
+    document.querySelectorAll(".sidenav a, .topnav a").forEach(function (elm) {
+        elm.addEventListener("click", function (event) {
+
+            const sidenav = document.querySelector(".sidenav");
+            M.Sidenav.getInstance(sidenav).close();
+
+            page = event.target.getAttribute("href").substr(1);
+            loadPageContent(page);
+        });
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-    // Activate sidebar nav
+
     var elems = document.querySelectorAll(".sidenav");
     M.Sidenav.init(elems);
-    loadNav();
 
-    function loadNav() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4) {
-                if (this.status != 200) return;
+    // loadNav();
+    loadNavigation();
 
-                // Muat daftar tautan menu
-                document.querySelectorAll(".topnav, .sidenav").forEach(function (elm) {
-                    elm.innerHTML = xhttp.responseText;
-                });
+    // function loadNav() {
+    //     var xhttp = new XMLHttpRequest();
+    //     xhttp.onreadystatechange = function () {
+    //         if (this.readyState == 4) {
+    //             if (this.status != 200) return;
 
-                // Daftarkan event listener untuk setiap tautan menu
-                document.querySelectorAll(".sidenav a, .topnav a").forEach(function (elm) {
-                    elm.addEventListener("click", function (event) {
-                        // Tutup sidenav
-                        var sidenav = document.querySelector(".sidenav");
-                        M.Sidenav.getInstance(sidenav).close();
+    //             document.querySelectorAll(".topnav, .sidenav").forEach(function (elm) {
+    //                 elm.innerHTML = xhttp.responseText;
+    //             });
 
-                        // Muat konten halaman yang dipanggil
-                        page = event.target.getAttribute("href").substr(1);
-                        loadPage(page);
-                    });
-                });
+    // document.querySelectorAll(".sidenav a, .topnav a").forEach(function (elm) {
+    //     elm.addEventListener("click", function (event) {
 
-            }
-        };
-        xhttp.open("GET", "nav.html", true);
-        xhttp.send();
-    }
+    //         var sidenav = document.querySelector(".sidenav");
+    //         M.Sidenav.getInstance(sidenav).close();
+
+    //         page = event.target.getAttribute("href").substr(1);
+    //         loadPage(page);
+    //     });
+    // });
+
+    //         }
+    //     };
+    //     xhttp.open("GET", "nav.html", true);
+    //     xhttp.send();
+    // }
+
 
     var page = window.location.hash.substr(1);
     if (page == "") page = "home";
-    loadPage(page);
 
-    function loadPage(page) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4) {
-                var content = document.querySelector("#body-content");
-                if (this.status == 200) {
-                    content.innerHTML = xhttp.responseText;
-                } else if (this.status == 404) {
-                    content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
-                } else {
-                    content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
-                }
-            }
-        };
-        xhttp.open("GET", "pages/" + page + ".html", true);
-        xhttp.send();
-    }
+    // loadPage(page);
+    loadPageContent(page);
+
+    // function loadPage(page) {
+    //     var xhttp = new XMLHttpRequest();
+    //     xhttp.onreadystatechange = function () {
+    //         if (this.readyState == 4) {
+    //             var content = document.querySelector("#body-content");
+    //             if (this.status == 200) {
+    //                 content.innerHTML = xhttp.responseText;
+    //             } else if (this.status == 404) {
+    //                 content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
+    //             } else {
+    //                 content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
+    //             }
+    //         }
+    //     };
+    //     xhttp.open("GET", "pages/" + page + ".html", true);
+    //     xhttp.send();
+    // }
+
 });
