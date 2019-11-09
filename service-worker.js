229a3b90a623e47d9a8a62caf6cf9f7435879167
Danis Zaidan <daniszaidan@gmail.com>
2019-11-09 14:51:17 +0700

    change cache name and function to delete old cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "firstpwa";
+const CACHE_NAME = "firstpwa-v1";
 var urlsToCache = [
     "/",
     "/nav.html",
@@ -25,15 +25,30 @@ self.addEventListener("fetch", function (event) {
             .match(event.request, { cacheName: CACHE_NAME })
             .then(function (response) {
                 if (response) {
-                    // console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+                    console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
                     return response;
                 }
 
-                // console.log(
-                //     "ServiceWorker: Memuat aset dari server: ",
-                //     event.request.url
-                // );
+                console.log(
+                    "ServiceWorker: Memuat aset dari server: ",
+                    event.request.url
+                );
                 return fetch(event.request);
             })
+    );
+});
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName != CACHE_NAME) {
+                        console.log("ServiceWorker: cache " + cacheName + " dihapus");
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
